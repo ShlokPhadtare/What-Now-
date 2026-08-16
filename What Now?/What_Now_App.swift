3417c0e99d77794027692eb5d12629068ef1d192
shlok <shlokphadtare9@gmail.com>
2026-08-16 18:17:32 +0530

    Initial Commit

---
+++ What Now?/What_Now_App.swift
@@ -0,0 +1,17 @@
+//
+//  What_Now_App.swift
+//  What Now?
+//
+//  Created by Shlok Phadtare on 16/08/26.
+//
+
+import SwiftUI
+
+@main
+struct What_Now_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
